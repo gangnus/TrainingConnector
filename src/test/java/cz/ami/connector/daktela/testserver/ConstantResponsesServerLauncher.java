64ddd38d63ddfef719676d882628c1a99691db52
Petr Gangnus <gangnus@komix.cz>
2022-06-20 23:43:38 +0200

    test server with memory for full imitation of the real Daktela Server. Tested and debbugged only for user creation and all users reading

--- src/test/java/cz/ami/connector/daktela/testserver/ConstantResponsesServerLauncher.java
+++ src/test/java/cz/ami/connector/daktela/testserver/ConstantResponsesServerLauncher.java
@@ -2,7 +2,7 @@ package cz.ami.connector.daktela.testserver;
 
 public class ConstantResponsesServerLauncher {
     static public void main(String[] arg) throws Exception {
-        TSWithConstantResponses.createServerForTesting();
+        TSWithMemory.createServerForTesting();
         while(true){
             Thread.sleep(100);
         }
